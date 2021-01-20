4488b8777226c3eac05ed798f14101705ab774cc
Limingbo <675683354@qq.com>
2021-01-20 23:18:01 +0800

    封装MapRole

---
+++ app/src/main/java/com/example/autopieces/map/MapRole.kt
@@ -0,0 +1,15 @@
+package com.example.autopieces.map
+
+import android.view.View
+import com.example.autopieces.role.Role
+
+/**
+ * 地图中一个角色
+ */
+class MapRole(
+    var role:Role,
+    var roleView:View,
+    var cell:Cell
+){
+
+}
